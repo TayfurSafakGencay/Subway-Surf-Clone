0dfc0f244751cc1fa48acc30bda201b0ae80310c
TayfurSafakGencay <safak.gencay@gmail.com>
2024-02-13 21:18:54 +0300

    Camera Follow

---
+++ Assets/Scripts/Runtime/Contexts/Main/View/CameraFollow/CameraFollowMediator.cs
@@ -0,0 +1,41 @@
+﻿using Runtime.Contexts.Main.Enum;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.mediation.impl;
+using UnityEngine;
+
+namespace Runtime.Contexts.Main.View.CameraFollow
+{
+  public class CameraFollowMediator : EventMediator
+  {
+    [Inject]
+    public CameraFollowView view { get; set; }
+
+    public override void OnRegister()
+    {
+      dispatcher.AddListener(MainEventKey.PlayerCreated, OnPlayerCreated);
+    }
+    
+    private void Update()
+    {
+      if (!view.startFollow)
+        return;
+      
+      Vector3 targetPosition = view.Player.position + view.OffSet;
+      targetPosition.z = 0;
+      transform.position = targetPosition;
+    }
+
+    private void OnPlayerCreated(IEvent payload)
+    {
+      GameObject player = (GameObject)payload.data;
+
+      view.Player = player.transform;
+      view.OffSet = transform.position - view.Player.position;
+      view.startFollow = true;
+    }
+    public override void OnRemove()
+    {
+      dispatcher.RemoveListener(MainEventKey.PlayerCreated, OnPlayerCreated);
+    }
+  }
+}
---
+++ Assets/Scripts/Runtime/Contexts/Main/View/CameraFollow/CameraFollowView.cs
@@ -0,0 +1,15 @@
+﻿using strange.extensions.mediation.impl;
+using UnityEngine;
+
+namespace Runtime.Contexts.Main.View.CameraFollow
+{
+  public class CameraFollowView : EventView
+  {
+    public Transform Player;
+
+    public Vector3 OffSet;
+
+    [HideInInspector]
+    public bool startFollow;
+  }
+}
