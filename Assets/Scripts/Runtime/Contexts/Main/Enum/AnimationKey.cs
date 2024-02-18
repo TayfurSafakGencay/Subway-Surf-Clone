ee9f69c2f60e431403263c38fbb4d1f249a99707
TayfurSafakGencay <safak.gencay@gmail.com>
2024-02-18 12:39:36 +0300

    Animations

---
+++ Assets/Scripts/Runtime/Contexts/Main/Enum/AnimationKey.cs
@@ -0,0 +1,13 @@
+﻿namespace Runtime.Contexts.Main.Enum
+{
+  public class AnimationKey
+  {
+    public const string Run = "Run";
+    public const string Slide = "Slide";
+    public const string Jump = "Jump";
+    public const string Idle = "Idle";
+    public const string Falling = "Falling";
+    public const string Landing = "Landing";
+    public const string Death = "Death";
+  }
+}
---
+++ Assets/Scripts/Runtime/Contexts/Main/Enum/SoundKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Runtime.Contexts.Main.Enum
+{
+  [Serializable]
+  public enum SoundKey
+  {
+    Coin,
+    Music,
+    Collision,
+  }
+}
